3b1dc2ce3753249d639ec295577ed9680ed1d3a2
Zoe Ames <zoeames@gmail.com>
2019-03-05 20:25:39 -0600

    finished dinosaurs

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 const dinosaurs = [
   {
     dinoType: 't-rex',
+    name: 'wrex',
   },
   {
     dinoType: 'stegosaurus',
+    name: 'steve',
   },
   {
     dinoType: 'velociraptor',
+    name: 'Ted',
   }
 ];
 
@@ -22,7 +25,10 @@ const printToDom = (divId, textToPrint) => {
 const buildDinosaurs = () => {
   let domString = '';
   for(let i = 0; i < dinosaurs.length; i++){
-    domString += `<h3>${dinosaurs[i].dinoType}</h3>`;
+    domString += `<div class="dinosaur">`
+    domString +=   `<h3>${dinosaurs[i].dinoType}</h3>`;
+    domString +=   `<p>${dinosaurs[i].name}</p>`
+    domString += `</div>`
   }
   printToDom('dino-barn', domString)
 };
